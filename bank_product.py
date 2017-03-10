2af465ec6815704a45b67c394ab2324acf8bf402
student <student>
2017-03-10 11:02:57 +0100

    Add id to bank product

--- bank_product.py
+++ bank_product.py
@@ -1,7 +1,8 @@
 class BankProduct:
 
-    def __init__(self, user_id):
+    def __init__(self, user_id, product_id):
         self._user_id = user_id
+        self._product_id = product_id
 
     def close_product(self):
         pass
