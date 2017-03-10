d2a1f6ed02d6b60e3c3763bdee6fc516a7d928bd
student <student>
2017-03-10 10:54:44 +0100

    Add printing information about bank account

--- bank_account.py
+++ bank_account.py
@@ -4,6 +4,7 @@ from bank_product import BankProduct
 class BankAccount(BankProduct):
 
     def __init__(self, starting_money):
+        print('Creating new bank account with ' + str(starting_money))
         self._account_balance = starting_money
 
     def close_product(self):
@@ -13,10 +14,13 @@ class BankAccount(BankProduct):
         if money > self._account_balance:
             print('Not enough money for withdraw')
         self._account_balance -= money
+        print('Withdraw ' + str(money))
         return money
 
     def deposit_money(self, money):
+        print('Deposit ' + str(money))
         self._account_balance += money
 
     def current_account_balance(self):
+        print('Current account balance is ' + str(self._account_balance))
         return self._account_balance
