ac00942a1df8bc3d256b386c6a0eba7556598555
Dawid Wiśniewski <dawid.wisniewski2013@outlook.com>
2017-03-17 01:26:29 +0100

    Basic credit implementation

--- credit.py
+++ credit.py
@@ -1,7 +1,27 @@
 from bank_product import BankProduct
+import time
 
 
 class Credit(BankProduct):
+    def __init__(self, user_id, product_id, money, account, end_date):
+        BankProduct.__init__(self, user_id=user_id, product_id=product_id, type='credit')
+        self._money = money
+        self._account = account
+        self._begin_date = time.asctime(time.localtime(time.time()))
+        self._end_date = end_date
+        account.deposit_money(money)
 
-    def close_product(self):
-        pass
+    def close_product(self, interests):
+        self._account.withdraw(self.money+(self.money*interests))
+
+    def get_credit_value(self):
+        return self._money
+
+    def get_account(self):
+        return self._account
+
+    def get_begin_date(self):
+        return self._begin_date
+
+    def get_end_date(self):
+        return self._end_date
