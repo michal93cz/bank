606997e514ce9c849f3bf34dc4b90275ada990ac
Dawid Wiśniewski <dawid.wisniewski2013@outlook.com>
2017-03-17 01:04:56 +0100

    Basic reports (sum of founds in bank and sum of debits)

--- report.py
+++ report.py
@@ -1,3 +1,23 @@
+from bank_account import BankAccount
+
+
 class Report:
-    def makeReport(self, product):
-        return product
+    @staticmethod
+    def sum_of_founds_in_bank(products):
+        founds_sum = 0
+        for product in products:
+            if type(product) is BankAccount:
+                founds_sum += product.current_account_balance
+        return founds_sum
+
+    def sum_of_transactions(self, history, begin_date, end_date):
+        pass
+
+    @staticmethod
+    def sum_of_debits(products):
+        debits_sum = 0
+        for product in products:
+            if type(product) is BankAccount:
+                if product.current_account_balance < 0:
+                    debits_sum += -product.current_account_balance
+        return debits_sum
