c263cb0912e85c52efeb9c9bf12774c05a357b89
Bartosz Litwiniuk <Bartosz.Litwiniuk@poczta.onet.pl>
2017-04-02 20:36:29 +0200

    Modify credit interest, add parameters

--- Interests/interest.py
+++ Interests/interest.py
@@ -4,6 +4,10 @@ from abc import ABCMeta, abstractmethod
 class Interest:
     __metaclass__ = ABCMeta
 
+    def __init__(self, value, percent):
+        self.value = value
+        self.percent = percent
+
+
     @abstractmethod
     def get_interests_value(self, money): raise NotImplemented
-
