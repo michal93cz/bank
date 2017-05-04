303974926fed21764416241fc815e520e3adf8cc
Dawid Wiśniewski <dawid.wisniewski2013@outlook.com>
2017-05-04 22:25:34 +0200

    Fixes for DebitBankAccount

--- tests/test_bank_account.py
+++ tests/test_bank_account.py
@@ -1,51 +1,52 @@
 import unittest
 from bank_account import BankAccount
+from debit_bank_account import DebitBankAccount
 
 
 class BankAccountTestCase(unittest.TestCase):
     def setUp(self):
-        self.account = BankAccount(1, 2, 50)
+        self.account = DebitBankAccount(BankAccount(1, 2, 1))
 
     def test_close_product_without_founds(self):
         self.assertEqual(self.account.close_product(), True, 'Cannot close account without money')
 
     def test_close_product_with_debit(self):
-        self.account.withdraw(25)
+        self.account.subtract(25)
         self.assertEqual(self.account.close_product(), False, 'Closing account with debit')
 
     def test_close_product_with_founds(self):
-        self.account.deposit(100)
+        self.account.add(100)
         self.assertEqual(self.account.close_product(), True, 'Cannot close account with money')
 
     def test_withdraw_twenty_five_from_empty_account(self):
         money = 25
-        result = self.account.withdraw(money)
-        self.assertEqual(result, money)
-        self.assertEqual(self.account.current_account_balance(), -money)
+        result = self.account.subtract(money)
+        self.assertEqual(result, True)
+        self.assertEqual(self.account.get_account_balance(), -money)
 
     def test_withdraw_sixty_from_empty_account(self):
         money = 60
-        result = self.account.withdraw(money)
-        self.assertEqual(self.account.current_account_balance(), 0)
+        result = self.account.subtract(money)
+        self.assertEqual(self.account.get_account_balance(), 0)
         self.assertEqual(result, 0)
 
     def test_withdraw_hundread_form_seventy_five(self):
         money = 100
         deposit = 75
-        self.account.deposit(deposit)
-        result = self.account.withdraw(money)
-        self.assertEqual(result, money)
-        self.assertEqual(self.account.current_account_balance(), deposit-money)
+        self.account.add(deposit)
+        result = self.account.subtract(money)
+        self.assertEqual(result, True)
+        self.assertEqual(self.account.get_account_balance(), deposit-money)
 
     def test_deposit_money(self):
         money = 20
-        current_balance = self.account.current_account_balance()
-        self.account.deposit(money)
-        self.assertEqual(self.account.current_account_balance(), money+current_balance)
+        current_balance = self.account.get_account_balance()
+        self.account.add(money)
+        self.assertEqual(self.account.get_account_balance(), money+current_balance)
 
     def test_deposit_money_with_debit(self):
         money = 20
-        self.account.withdraw(money)
-        current_balance = self.account.current_account_balance()
-        self.account.deposit(money*2)
-        self.assertEqual(self.account.current_account_balance(), money*2+current_balance)
+        self.account.add(money)
+        current_balance = self.account.get_account_balance()
+        self.account.add(money*2)
+        self.assertEqual(self.account.get_account_balance(), money*2+current_balance)
